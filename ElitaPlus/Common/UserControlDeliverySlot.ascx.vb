303a95f6ca5b2331cae00f30be25ca8b8cb723e7
Pavan Poreddy <pavan.poreddy@assurant.com>
2019-07-22 14:40:05 -0400

    Bug 351417:US 337347_Japan Project River: Delivery details gets displayed in Upper Case instead of Camel case.

--- ElitaPlus/Common/UserControlDeliverySlot.ascx.vb
+++ ElitaPlus/Common/UserControlDeliverySlot.ascx.vb
@@ -125,6 +125,13 @@ Public Class UserControlDeliverySlot
 #Region "Control Event"
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
     End Sub
+
+    Public Sub TranslateLabels()
+        lblCourierProduct.Text = TranslationBase.TranslateLabelOrMessage("COURIER_PRODUCT")
+        lblAvailableDeliveryTiming.Text = TranslationBase.TranslateLabelOrMessage("AVAILABLE_DELIVERY_TIMING")
+        lblDesiredDate.Text = TranslationBase.TranslateLabelOrMessage("DESIRED_DELIVERY_DATE")
+        lblDeliverySlot.Text = TranslationBase.TranslateLabelOrMessage("DESIRED_DELIVERY_TIME")
+    End Sub
 #End Region
 #Region "Other Event"
     Private Function ValidateSetDeliveryDate(ByVal dtvalue As String) As Boolean
@@ -141,25 +148,25 @@ Public Class UserControlDeliverySlot
 
             Catch ex As Exception
                 Page.MasterPage.MessageController.AddError(TranslationBase.TranslateLabelOrMessage("DESIRED_DELIVERY_DATE") & " - " & txtDeliveryDate.Text & " : " & TranslationBase.TranslateLabelOrMessage(Messages.INVALID_DATE_ERR), False)
-                DeliveryDate = nothing 'clear selection
+                DeliveryDate = Nothing 'clear selection
                 Return False
             End Try
 
             If desiredDeliveryDate < Date.Now.Date Then
                 Page.MasterPage.MessageController.AddError(TranslationBase.TranslateLabelOrMessage("DESIRED_DELIVERY_DATE") & " - " & txtDeliveryDate.Text & " : " & TranslationBase.TranslateLabelOrMessage(Messages.NO_PAST_DATE), False)
-                DeliveryDate = nothing 'clear selection
+                DeliveryDate = Nothing 'clear selection
                 Return False
-            elseif State.CurrentEstimate.Behavior.AllowSelectionAfterLastDeliveryDate = False Then 'check the date is within the available dates
-                if state.CurrentEstimate.AvailableDeliveryDays.ToList().Exists(Function(x) x.DeliveryDate = desiredDeliveryDate) = False Then
+            ElseIf State.CurrentEstimate.Behavior.AllowSelectionAfterLastDeliveryDate = False Then 'check the date is within the available dates
+                If State.CurrentEstimate.AvailableDeliveryDays.ToList().Exists(Function(x) x.DeliveryDate = desiredDeliveryDate) = False Then
                     Page.MasterPage.MessageController.AddError(TranslationBase.TranslateLabelOrMessage("DESIRED_DELIVERY_DATE") & " - " & txtDeliveryDate.Text & " : " & TranslationBase.TranslateLabelOrMessage(Messages.INVALID_DATE_ERR), False)
-                    DeliveryDate = nothing 'clear selection
+                    DeliveryDate = Nothing 'clear selection
                     Return False
-                end If
+                End If
             End If
             DeliveryDate = desiredDeliveryDate
         Else
-            If DeliveryDate.HasValue then 
-                DeliveryDate = nothing
+            If DeliveryDate.HasValue Then
+                DeliveryDate = Nothing
             End If
 
             ddlDeliverySlots.Items.Clear() ' remove the delivery slot options
@@ -190,28 +197,28 @@ Public Class UserControlDeliverySlot
         End Try
     End Sub
 
-    Private sub SetControlStatus()
+    Private Sub SetControlStatus()
         If chkNotSpecify.Checked = True Then
             ResetToDefaultSetting()
         Else
             EnableDisableControl(True)
-            
-            if State.CurrentEstimate.Behavior.AllowSelectionAfterLastDeliveryDate Then
+
+            If State.CurrentEstimate.Behavior.AllowSelectionAfterLastDeliveryDate Then
                 ' enable dates starting from the fastest delivery date
                 Dim fastestDeliveryDate As DeliveryDay = (From delDay As DeliveryDay In State.CurrentEstimate.AvailableDeliveryDays Select delDay Order By delDay.DeliveryDate Ascending).First()
-                ElitaPlusPage.AddCalendarNewWithDisableBeforeDate(imageBtnDeliveryDate, txtDeliveryDate, "", "N", fastestDeliveryDate.DeliveryDate) 
-            Else 
+                ElitaPlusPage.AddCalendarNewWithDisableBeforeDate(imageBtnDeliveryDate, txtDeliveryDate, "", "N", fastestDeliveryDate.DeliveryDate)
+            Else
                 ' only enable those dates in the delivery days list
                 Dim deliveryDays As New List(Of Date)
-                for each dd as DeliveryDay In State.CurrentEstimate.AvailableDeliveryDays
+                For Each dd As DeliveryDay In State.CurrentEstimate.AvailableDeliveryDays
                     deliveryDays.Add(dd.DeliveryDate)
-                Next            
-                ElitaPlusPage.AddCalendarNewWithEnableDates(imageBtnDeliveryDate, txtDeliveryDate, "", "N", deliveryDays)            
-            End If            
+                Next
+                ElitaPlusPage.AddCalendarNewWithEnableDates(imageBtnDeliveryDate, txtDeliveryDate, "", "N", deliveryDays)
+            End If
         End If
-    End sub
+    End Sub
     Protected Sub chkNotSpecify_CheckedChanged(sender As Object, e As EventArgs) Handles chkNotSpecify.CheckedChanged
-        SetControlStatus()        
+        SetControlStatus()
     End Sub
 #End Region
 #Region "Other Function"
@@ -249,24 +256,24 @@ Public Class UserControlDeliverySlot
 
         Dim wsRequest As New GetDeliverySlotsRequest
 
-        If String.IsNullOrWhiteSpace(State.CountryCode) OrElse state.DeliveryAddress is nothing OrElse String.IsNullOrWhiteSpace(state.DeliveryAddress.PostalCode) Then
+        If String.IsNullOrWhiteSpace(State.CountryCode) OrElse State.DeliveryAddress Is Nothing OrElse String.IsNullOrWhiteSpace(State.DeliveryAddress.PostalCode) Then
             Page.MasterPage.MessageController.AddError(Message.MSG_ERR_COUNTRY_POSTAL_MANDATORY, True)
             Exit Sub
         End If
 
         wsRequest.CountryCode = State.CountryCode
-        wsRequest.ServiceCenterCode = state.ServiceCenter
+        wsRequest.ServiceCenterCode = State.ServiceCenter
         wsRequest.CourierCode = State.CourierCode
-        wsRequest.CourierProductCode = state.CourierProductCode
-        wsRequest.DeliveryAddress = New Address() with {
+        wsRequest.CourierProductCode = State.CourierProductCode
+        wsRequest.DeliveryAddress = New Address() With {
             .CountryCode = State.DeliveryAddress.CountryCode,
-            .PostalCode = state.DeliveryAddress.PostalCode,
-            .StateCode = state.DeliveryAddress.RegionShortDesc,
+            .PostalCode = State.DeliveryAddress.PostalCode,
+            .StateCode = State.DeliveryAddress.RegionShortDesc,
             .City = State.DeliveryAddress.City,
             .Address1 = State.DeliveryAddress.Address1,
             .Address2 = State.DeliveryAddress.Address2,
             .Address3 = State.DeliveryAddress.Address3}
-        
+
         Try
             Dim wsResponse As GetDeliverySlotsResponse = WcfClientHelper.Execute(Of WebAppGatewayClient, WebAppGateway, GetDeliverySlotsResponse)(
                                                                                 GetClaimFulfillmentWebAppGatewayClient(),
@@ -291,7 +298,7 @@ Public Class UserControlDeliverySlot
         End Try
     End Sub
 
-     Private Sub ShowDeliveryEstimate()
+    Private Sub ShowDeliveryEstimate()
 
         If String.IsNullOrEmpty(State.CurrentEstimate.CourierCode) = True Then
             trCourierProduct.Attributes("style") = "display: none"
@@ -300,14 +307,14 @@ Public Class UserControlDeliverySlot
         If State.CurrentEstimate.Behavior.UseDeliverySlot = False Then
             trDeliverySlot.Attributes("style") = "display: none"
         End If
-        
+
         EnableDisableNotSpecifyCheckBox()
         ShowDeliveryTimeRange(State.CurrentEstimate)
     End Sub
 
     Private Sub EnableDisableNotSpecifyCheckBox()
         ''if selection is not allowed, then uncheck and disable the check box
-        If (Not State Is Nothing andAlso Not State.CurrentEstimate Is Nothing AndAlso Not State.CurrentEstimate.Behavior Is Nothing) Then
+        If (Not State Is Nothing AndAlso Not State.CurrentEstimate Is Nothing AndAlso Not State.CurrentEstimate.Behavior Is Nothing) Then
             chkNotSpecify.Checked = True
 
             If (State.CurrentEstimate.Behavior.SelectionAllowed = False) Then
@@ -325,7 +332,7 @@ Public Class UserControlDeliverySlot
         Dim blnUseCourier As Boolean = False
         State.CurrentEstimate = State.DeliveryDateList.First()
 
-       If String.IsNullOrEmpty(State.CurrentEstimate.CourierCode) = False Then
+        If String.IsNullOrEmpty(State.CurrentEstimate.CourierCode) = False Then
             blnUseCourier = True
         End If
 
@@ -343,7 +350,7 @@ Public Class UserControlDeliverySlot
         End If
 
         ShowDeliveryEstimate()
-        
+
     End Sub
     Private Sub ShowFaultException(ByVal fex As FaultException)
         If fex IsNot Nothing Then
@@ -362,9 +369,9 @@ Public Class UserControlDeliverySlot
             End If
         End If
     End Sub
-    
 
-    Private Sub ShowDeliveryTimeRange(ByVal de As DeliveryEstimate )
+
+    Private Sub ShowDeliveryTimeRange(ByVal de As DeliveryEstimate)
         Dim fastestDeliveryDateTime As String = String.Empty
         Dim fastestDeliveryDate As DeliveryDay = (From delDay As DeliveryDay In de.AvailableDeliveryDays Select delDay Order By delDay.DeliveryDate Ascending).First()
         If fastestDeliveryDate IsNot Nothing Then
@@ -375,8 +382,8 @@ Public Class UserControlDeliverySlot
                 fDeliveryDate = ElitaPlusPage.GetDateFormattedStringNullable(fastestDeliveryDate.DeliveryDate)
             End If
 
-            if de.Behavior.UseDeliverySlot Then 'add slot information 
-                If fastestDeliveryDate.DeliverySlots IsNot Nothing AndAlso fastestDeliveryDate.DeliverySlots.Length > 0 AndAlso de.Behavior.SelectionAllowed  Then
+            If de.Behavior.UseDeliverySlot Then 'add slot information 
+                If fastestDeliveryDate.DeliverySlots IsNot Nothing AndAlso fastestDeliveryDate.DeliverySlots.Length > 0 AndAlso de.Behavior.SelectionAllowed Then
                     Dim fastestDeliveryTimeSlot As DeliverySlot = (From delSlot As DeliverySlot In fastestDeliveryDate.DeliverySlots Select delSlot Order By delSlot.Sequence Ascending).First()
                     If fastestDeliveryTimeSlot IsNot Nothing Then
                         fastestDeliveryDateTime = If(LookupListNew.GetDescriptionFromCode(LookupListNew.LK_DESIRED_DELIVERY_TIME_SLOT, fastestDeliveryTimeSlot.Description, Authentication.CurrentUser.LanguageId), fastestDeliveryTimeSlot.Description) + " " + fDeliveryDate
@@ -386,9 +393,9 @@ Public Class UserControlDeliverySlot
                 Else
                     fastestDeliveryDateTime = TranslationBase.TranslateLabelOrMessage("TIME_SLOT_NOT_APPLICABLE") + " " + fDeliveryDate
                 End If
-            else
+            Else
                 fastestDeliveryDateTime = fDeliveryDate
-            End If            
+            End If
         End If
 
         Dim lastDeliveryDateTime As String = String.Empty
@@ -401,8 +408,8 @@ Public Class UserControlDeliverySlot
                 lDeliveryDate = ElitaPlusPage.GetDateFormattedStringNullable(lastDeliveryDate.DeliveryDate)
             End If
 
-            if de.Behavior.UseDeliverySlot Then 'add slot information 
-                If lastDeliveryDate.DeliverySlots IsNot Nothing AndAlso lastDeliveryDate.DeliverySlots.Length > 0 AndAlso de.Behavior.SelectionAllowed  Then
+            If de.Behavior.UseDeliverySlot Then 'add slot information 
+                If lastDeliveryDate.DeliverySlots IsNot Nothing AndAlso lastDeliveryDate.DeliverySlots.Length > 0 AndAlso de.Behavior.SelectionAllowed Then
                     Dim lastDeliveryTimeSlot As DeliverySlot = (From delSlot As DeliverySlot In lastDeliveryDate.DeliverySlots Select delSlot Order By delSlot.Sequence Ascending).Last()
                     If lastDeliveryTimeSlot IsNot Nothing Then
                         lastDeliveryDateTime = If(LookupListNew.GetDescriptionFromCode(LookupListNew.LK_DESIRED_DELIVERY_TIME_SLOT, lastDeliveryTimeSlot.Description, Authentication.CurrentUser.LanguageId), lastDeliveryTimeSlot.Description) + " " + lDeliveryDate
@@ -412,9 +419,9 @@ Public Class UserControlDeliverySlot
                 Else
                     lastDeliveryDateTime = TranslationBase.TranslateLabelOrMessage("TIME_SLOT_NOT_APPLICABLE") + " " + lDeliveryDate
                 End If
-            else
+            Else
                 lastDeliveryDateTime = lDeliveryDate
-            End If            
+            End If
         End If
         AvailableDeliveryDateTiming = fastestDeliveryDateTime + " ~ " + lastDeliveryDateTime
     End Sub
@@ -422,16 +429,16 @@ Public Class UserControlDeliverySlot
     Private Sub ResetToDefaultSetting()
         txtDeliveryDate.Text = String.Empty
 
-        If DeliveryDate.HasValue then 
-            DeliveryDate = nothing
+        If DeliveryDate.HasValue Then
+            DeliveryDate = Nothing
         End If
 
         ddlDeliverySlots.Items.Clear()
-        if chkNotSpecify.Checked Then 'Not specified checked, disable controls
+        If chkNotSpecify.Checked Then 'Not specified checked, disable controls
             EnableDisableControl(False)
-        Else 
+        Else
             EnableDisableControl(True) 'Not specified checked, enable controls
-        End If        
+        End If
     End Sub
 
     Private Sub EnableDisableControl(ByVal bEnable As Boolean)
@@ -446,23 +453,23 @@ Public Class UserControlDeliverySlot
     End Sub
 
     Protected Sub ddlCourierProduct_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ddlCourierProduct.SelectedIndexChanged
-        State.CurrentEstimate = state.DeliveryDateList.FirstOrDefault(Function(de) $"{de.CourierCode}{de.CourierProductCode}" = ddlCourierProduct.SelectedValue)
+        State.CurrentEstimate = State.DeliveryDateList.FirstOrDefault(Function(de) $"{de.CourierCode}{de.CourierProductCode}" = ddlCourierProduct.SelectedValue)
         ShowDeliveryEstimate()
     End Sub
 
     Private Sub UserControlDeliverySlot_PreRender(sender As Object, e As EventArgs) Handles Me.PreRender
         ' hide courier and delivery slot based on estimate data
-        if not State.CurrentEstimate Is nothing andalso string.IsNullOrEmpty(State.CurrentEstimate.CourierCode) = true then
+        If Not State.CurrentEstimate Is Nothing AndAlso String.IsNullOrEmpty(State.CurrentEstimate.CourierCode) = True Then
             trCourierProduct.Attributes("style") = "display: none"
         End If
 
-        if not State.CurrentEstimate Is nothing AndAlso State.CurrentEstimate.Behavior.UseDeliverySlot = False Then
+        If Not State.CurrentEstimate Is Nothing AndAlso State.CurrentEstimate.Behavior.UseDeliverySlot = False Then
             trDeliverySlot.Attributes("style") = "display: none"
         End If
 
-        if State.DeliveryDateList is nothing OrElse State.DeliveryDateList.Count() = 0 Then 'No date returned
+        If State.DeliveryDateList Is Nothing OrElse State.DeliveryDateList.Count() = 0 Then 'No date returned
             Visible = False
-        End If        
+        End If
     End Sub
 
 #End Region
