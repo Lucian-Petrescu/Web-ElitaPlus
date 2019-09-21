8759c2c1b1b85fbe48aa596dbf5266c35628af2d
Vemkata Balla <vemkata.prasad.balla@assurant.com>
2019-09-21 00:09:57 -0400

    Bug 381229:US_351967_360995_KDDI_Expedited_ River __Incorrect value for Expected delivery date and time

--- ElitaPlus/Claims/ClaimRecordingForm.aspx.vb
+++ ElitaPlus/Claims/ClaimRecordingForm.aspx.vb
@@ -2430,12 +2430,17 @@ Public Class ClaimRecordingForm
                             If Not lOption.LogisticOptionInfo Is Nothing Then
                                 If State.LogisticsOption.Code.ToUpper().Equals("X") Then
                                     Dim DeliverySlotDescr As String = ucDeliverySlots.DeliverySlotDescription
+                                    Dim thisTime As Date = Date.Now
+                                    Dim timezne As TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time")
+                                    Dim localExpectedDeliveryTime As Date
 
                                     Select Case DeliverySlotDescr.ToUpper()
-                                        Case "EXP_DEL4"
-                                            lOption.LogisticOptionInfo.EstimatedChangedDeliveryDate = Date.Now.AddHours(4)
-                                        Case "EXP_DEL3"
-                                            lOption.LogisticOptionInfo.EstimatedChangedDeliveryDate = Date.Now.AddHours(3)
+                                        Case "EXP_DEL4"      ' For Japan Expected delivery time is 4 hrs from current Japan time
+                                            localExpectedDeliveryTime = TimeZoneInfo.ConvertTime(thisTime.AddHours(4), TimeZoneInfo.Local, timezne)
+                                            lOption.LogisticOptionInfo.EstimatedChangedDeliveryDate = localExpectedDeliveryTime
+                                        Case "EXP_DEL3"      ' For KDDI Expected delivery time is 3 hrs from current Japan time
+                                            localExpectedDeliveryTime = TimeZoneInfo.ConvertTime(thisTime.AddHours(3), TimeZoneInfo.Local, timezne)
+                                            lOption.LogisticOptionInfo.EstimatedChangedDeliveryDate = localExpectedDeliveryTime
                                     End Select
                                 Else
                                     lOption.LogisticOptionInfo.EstimatedChangedDeliveryDate = selectedDeliveryDate
