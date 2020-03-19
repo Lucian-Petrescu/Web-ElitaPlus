5d7c9fd9cc5e29efaf9680a8ccd2183e79e073a0
Piyush Bansal <piyush.bansal@assurant.com>
2020-03-19 17:13:40 -0400

    remove commented code

--- ElitaPlus/Tables/PriceListDetailForm.aspx.vb
+++ ElitaPlus/Tables/PriceListDetailForm.aspx.vb
@@ -134,7 +134,6 @@ Public Class PriceListDetailForm
         Public MyChildBO As PriceListDetail
         ' Public ScreenSnapShotChildBO As PriceListDetail
 
-        'Public MyChildVendorBO As VendorQuantity
         Public MyChildEquipmentBO As Equipment
         'Public ScreenSnapShotChildVendorBO As ServiceCenter
 
@@ -864,29 +863,11 @@ Public Class PriceListDetailForm
             Me.PopulateBOProperty(Me.State.MyChildBO, "PriceListDetailTypeId", New Guid(ddldetailtype.SelectedValue))
 
             'Populate the vendor quantity
-            'If Not Me.State.MyChildBO.GetVendorQuantiy().Equals(Guid.Empty) Then
-            '    Me.State.MyChildVendorBO = Me.State.MyBO.GetNewVendorQuantityChild(Me.State.MyChildBO.GetVendorQuantiy())
-            '    'Me.State.MyChildVendorBO = New VendorQuantity(Me.State.MyChildBO.GetVendorQuantiy())
-            'Else
-            '    Me.State.MyChildVendorBO = Me.State.MyBO.GetNewVendorQuantityChild()
-            '    'Me.State.MyChildVendorBO = New VendorQuantity()
-            '    Me.State.MyChildVendorBO.TableName = "PRICE_LIST_DETAIL"
-            '    Me.State.MyChildVendorBO.ReferenceId = Me.State.MyChildBO.Id
-            'End If
-
             If (txtNewItemQuantity.Text <> "") Then
                 Me.State.MyChildBO.VendorQuantity = CType(Me.txtNewItemQuantity.Text.Trim(), LongType)
             End If
 
-            'Me.State.MyChildVendorBO.Sku = Me.State.MyChildBO.VendorSku
-            'Me.State.MyChildVendorBO.SkuDescription = Me.State.MyChildBO.VendorSkuDescription
-            'Me.State.MyChildVendorBO.PriceListDetailID = Me.State.MyChildBO.Id
-
             Me.BindChildBoPropertiesToLabels()
-
-            'If Not Me.State.MyChildVendorBO.Quantity Is Nothing Then
-            '    Me.State.MyChildVendorBO.Validate()
-            'End If
 
             If Me.State.MyChildBO.IsDirty Then
                 ' check if already exists
@@ -914,19 +895,12 @@ Public Class PriceListDetailForm
                             Me.State.MyChildBO.EndEdit()
                         End If
 
-                        'If Me.State.MyChildVendorBO.IsDirty Then
-                        '    If Not Me.State.MyChildVendorBO.Quantity Is Nothing Then
-                        '        Me.State.MyChildVendorBO.Save()
-                        '        Me.State.MyChildVendorBO.EndEdit()
-                        '    End If
-                        'End If
                         Me.State.HasDataChanged = False
                     End If
 
                 Catch ex As Exception
                     Me.State.MyChildBO.RejectChanges()
                     Me.State.MyChildEquipmentBO.RejectChanges()
-                    'Me.State.MyChildVendorBO.RejectChanges()
                     Throw ex
                 End Try
 
@@ -1271,10 +1245,6 @@ Public Class PriceListDetailForm
                         BeginPriceListDetailChildEdit()
                     End If
 
-                    'If Not Me.State.MyChildBO.GetVendorQuantiy().Equals(Guid.Empty) Then
-                    '    Me.State.MyChildVendorBO = New VendorQuantity(Me.State.MyChildBO.GetVendorQuantiy())
-                    'End If
-
                     Try
                         If (Me.State.MyChildBO.Effective.Value <= PriceListDetail.GetCurrentDateTime()) Then
                             Me.State.MyChildBO.BeginEdit()
@@ -1282,11 +1252,6 @@ Public Class PriceListDetailForm
                             Me.State.MyChildBO.EndEdit()
                             Me.State.MyChildBO.Save()
                         ElseIf (Me.State.MyChildBO.Effective.Value > PriceListDetail.GetCurrentDateTime()) Then
-                            'If Not Me.State.MyChildBO.GetVendorQuantiy().Equals(Guid.Empty) Then
-                            '    Me.State.MyChildVendorBO.Delete()
-                            '    Me.State.MyChildVendorBO.Save()
-                            '    Me.State.MyChildVendorBO.EndEdit()
-                            'End If
                             Me.State.MyChildBO.Delete()
                             Me.State.MyChildBO.Save()
                             Me.State.MyChildBO.EndEdit()
@@ -1294,7 +1259,6 @@ Public Class PriceListDetailForm
                         End If
                     Catch ex As Exception
                         Me.State.MyChildBO.RejectChanges()
-                        'Me.State.MyChildVendorBO.RejectChanges()
                         Me.MasterPage.MessageController.AddError(ElitaPlus.ElitaPlusWebApp.Message.ERR_DELETING_DATA)
                         Throw ex
                     End Try
@@ -1745,9 +1709,6 @@ Public Class PriceListDetailForm
                 BeginPriceListDetailChildEdit()
                 Me.PopulateDetailFromPriceListDetailChildBO()
 
-                'If Not Me.State.MyChildBO.GetVendorQuantiy().Equals(Guid.Empty) Then
-                '    Me.State.MyChildVendorBO = New VendorQuantity(Me.State.MyChildBO.GetVendorQuantiy())
-                'End If
 
                 '' condition
                 'Me.PopulateControlFromBOProperty(ddlNewItemCondition, Me.State.MyChildBO.ConditionId)
