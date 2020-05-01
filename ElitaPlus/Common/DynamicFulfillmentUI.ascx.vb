d8e8708996ec334bb02b04ea8c4668ab036ad23e
Karthik Gangadharan <karthik.gangadharan@assurant.com>
2020-05-01 14:45:39 -0400

    Set DF UI culture to source system culture

--- ElitaPlus/Common/DynamicFulfillmentUI.ascx.vb
+++ ElitaPlus/Common/DynamicFulfillmentUI.ascx.vb
@@ -9,7 +9,7 @@
     Public ScriptUri As String
     Public AccessToken As String
     Public IsLoadError As Boolean
-
+    Public Culture As String = Threading.Thread.CurrentThread.CurrentCulture.Name
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
