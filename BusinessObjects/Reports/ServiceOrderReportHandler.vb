b635f7f10d59fafc45491673c2043d661c409448
Jalindar Tidke <jalindar.tidke@assurant.com>
2020-08-18 21:47:45 +0530

    updated State/Province addedd to cutomer address

--- BusinessObjects/Reports/ServiceOrderReportHandler.vb
+++ BusinessObjects/Reports/ServiceOrderReportHandler.vb
@@ -129,6 +129,10 @@ Public Class ServiceOrderReportHandler
                 soRow.ADDRESS3 = Address.Address3
                 soRow.CITY = Address.City
                 soRow.ZIP = Address.PostalCode
+                If Not Address.RegionId.Equals(Guid.Empty) Then
+                    soRow.STATE_PROVINCE = New Region(Address.RegionId).Description
+                    soRow.STATE_PROVINCE_CODE = New Region(Address.RegionId).ShortDesc
+                End If
 
             End If
             End If
