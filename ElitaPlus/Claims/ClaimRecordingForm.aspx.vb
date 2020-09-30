0da8daba578c12919b506b38af4d9c4582390ee2
Jalindar Tidke <jalindar.tidke@assurant.com>
2020-09-30 18:54:36 +0000

    Updated ClaimRecordingForm.aspx.vb

--- ElitaPlus/Claims/ClaimRecordingForm.aspx.vb
+++ ElitaPlus/Claims/ClaimRecordingForm.aspx.vb
@@ -557,9 +557,9 @@ Public Class ClaimRecordingForm
     ''' <returns>Instance of <see cref="ClaimRecordingServiceClient"/></returns>
     Private Shared Function GetClient() As ClaimRecordingServiceClient
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
-        Dim client = New ClaimRecordingServiceClient(EndPointName, "http://localhost:20377/ClaimRecordingService.svc") 'ConfigurationManager.AppSettings(ServiceUrl)
-        client.ClientCredentials.UserName.UserName = "elita1" 'ConfigurationManager.AppSettings(UserName)
-        client.ClientCredentials.UserName.Password = "elita1" 'ConfigurationManager.AppSettings(Password)
+        Dim client = New ClaimRecordingServiceClient(EndPointName,ConfigurationManager.AppSettings(ServiceUrl)) 
+        client.ClientCredentials.UserName.UserName = ConfigurationManager.AppSettings(UserName)
+        client.ClientCredentials.UserName.Password = ConfigurationManager.AppSettings(Password)
         Return client
     End Function
 
