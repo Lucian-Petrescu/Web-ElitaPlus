ec6a2e8726f21775f73ebb77cca3af71a9681bbe
uk0jko <yogita.palve@assurant.com>
2020-10-12 12:26:35 +0100

    PBI-576018

--- BusinessObjects/Tables/Accounting/AcctCompany.vb
+++ BusinessObjects/Tables/Accounting/AcctCompany.vb
@@ -387,8 +387,8 @@ Public Class AcctCompany
     End Function
     Public Function IsCompUsingNewAccounting(ByVal acctCompanyId As Guid) As Boolean
         Try
-            Dim objacctcompDAL As New AcctCompanyDAL
-            Dim ds As DataSet = objacctcompDAL.GetCompUsingNewAccForAccCompany(acctCompanyId)
+            Dim objacctcompDal As New AcctCompanyDAL
+            Dim ds As DataSet = objacctcompDal.GetCompUsingNewAccForAccCompany(acctCompanyId)
             If ds.Tables(0).Rows.Count > 0 Then
                 Return True
             Else
