7036b0f3979b192839d72783fd9532e19816f8b4
uk0jko <yogita.palve@assurant.com>
2020-10-19 16:26:50 +0100

    PBI-587032

--- ElitaHarvesterService/Tasks/CustCommunication/SendClaimGiftCardEMailTask.vb
+++ ElitaHarvesterService/Tasks/CustCommunication/SendClaimGiftCardEMailTask.vb
@@ -224,7 +224,7 @@ Public Class SendClaimGiftCardEMailTask
         Dim request As New Request
 
         Dim giftCardRequest As GenerateGiftCardRequest = PopulateCustomerAndGiftCardStaticInfo(oCertificate)
-        giftCardRequest.ReferenceNumber = oCertificate.CertNumber + DateTime.UtcNow.ToString("ddMMyy")
+        giftCardRequest.ReferenceNumber = oCertificate.CertNumber + DateTime.UtcNow.ToString("ddMMyyyy")
         giftCardRequest.OperationFor = GiftCardOperationCodeForVoucher
 
         request.GiftcardRequest = giftCardRequest
