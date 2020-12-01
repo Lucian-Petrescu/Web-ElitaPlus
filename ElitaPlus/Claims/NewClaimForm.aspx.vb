29ec50c75c95b286f34a8edd0f85311487914b0e
Nikhil Karandikar <nikhil.karandikar@assurant.com>
2020-12-01 21:25:50 +0530

    Bug 610141: ECI: Unexpected error message being received when processing questionnaire
    Related Work Items: #610141

--- ElitaPlus/Claims/NewClaimForm.aspx.vb
+++ ElitaPlus/Claims/NewClaimForm.aspx.vb
@@ -1920,8 +1920,7 @@ Partial Class NewClaimForm
         Dim price As Decimal = 0
         dv = Me.State.MyBO.GetRePairPricesByMethodOfRepair
 
-        If Not dv Is Nothing AndAlso dv.Table.Rows.Count > 0 Then
-            price = CDec(dv.Table.Rows(0)(COL_PRICE_DV))
+        If Not dv Is Nothing AndAlso dv.Table.Rows.Count > 0 AndAlso Not IsDBNull(dv.Table.Rows(0)(COL_PRICE_DV)) AndAlso Decimal.TryParse(dv.Table.Rows(0)(COL_PRICE_DV), price) Then
             Select Case dv.Table.Rows(0)(PriceListDetailDAL.COL_NAME_SERVICE_TYPE_CODE).ToString()
                 Case Codes.METHOD_OF_REPAIR_SEND_IN
                     nCarryInPrice = price 'CDec(dv.Table.Rows(0)(COL_PRICE_DV))
